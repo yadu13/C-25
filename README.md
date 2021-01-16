b44ee5aa22f10e2aa8c8f23fe6e7015cbcc9b67c
yadu13 <76615375+yadu13@users.noreply.github.com>
2021-01-16 17:02:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# angryBirdsStage2StudentActivity
-Student Activity Link for Angry Birds Stage 2
+# TopplingBoxes
+Toppling Boxes
--- sketch.js
+++ sketch.js
@@ -3,37 +3,39 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1;
+var Log1,Pig1;
+
+function preload(){
+bgImg=loadImage("sprites/bg.png");
+
+}
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
-
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+    ground = new Ground(600,height,1200,20)
+    Pig1= new Pig(810,350);
+    Log1=new Log(810,260,300,PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
+    Pig3 = new Pig(810,220);
+    Log3 =new Log(810,180,300,PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    Log5 =new Log(760,120,150,PI/7);
+    Log6 =new Log(870,120,150,PI/7);
 
     bird = new Bird(100,100);
-
 }
 
 function draw(){
-    background(0);
+    background(bgImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -41,17 +43,16 @@ function draw(){
     box1.display();
     box2.display();
     ground.display();
-    pig1.display();
-    log1.display();
+    Pig1.display();
+    Log1.display();
 
     box3.display();
     box4.display();
-    pig3.display();
-    log3.display();
+    Pig3.display();
+    Log3.display();
 
+    Log5.display();
+    Log6.display();
     box5.display();
-    log4.display();
-    log5.display();
-
     bird.display();
 }
